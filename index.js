cfbb9d8a5e1ecb4445e6b75c92fa8f13e37e6350
Hector <nandohector1@gmail.com>
2020-07-06 12:35:02 -0500

    cambiando puerto

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors =require('cors');
 
 const app = express(); //Crea el servidor
 app.use(cors())
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
